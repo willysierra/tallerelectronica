1b6d6ccba6abaae97186c7f60ecd0cc3b1d67de1
me.vieda372 <me.vieda372@3c8b30a8-bd7b-6e8a-6419-ac650d15a375>
2010-04-26 00:00:58 +0000

    Creación de estructura general del repositorio

---
+++ AVR/ControlDiplayExterno/ControlDiplayExterno.c
@@ -0,0 +1,37 @@
+/**
+ * -------------------------------------------------------------------
+ * Universidad de los Andes - Facultad de Ingenieria
+ * Departamento de Ingenieria Electrica y Electronica
+ * Proyecto Taller de Electrónica
+ * -------------------------------------------------------------------
+ *
+ * Archivo:				ControlDisplayExterno.c
+ * Fecha Creacion:		Abril 24 de 2010
+ * Fecha Modificacion:	Abril 25 de 2010
+ * Version (Revision):	0.1 (1)
+ *
+ * Descripcion:	Control diplay externo
+ *
+ */
+
+
+/* Definición de Constantes del Sistema */
+#define F_CPU 8000000UL
+
+/* Exportación de Librerias */
+#include <avr\io.h>
+#include <stdio.h>
+
+
+//  ------------------------------------------------------------------------
+//	VARIABLES GLOBALES DEL SISTEMA / BUFFERS
+//  ------------------------------------------------------------------------
+
+
+//  ------------------------------------------------------------------------
+//	PROGRAMA PRINCIPAL DEL SISITEMA
+//  ------------------------------------------------------------------------
+
+int main(void) {
+
+}
