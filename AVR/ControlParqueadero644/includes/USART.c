9b8d55a70a475c2a94286c998083fef53d27224d
me.vieda372 <me.vieda372@3c8b30a8-bd7b-6e8a-6419-ac650d15a375>
2010-05-21 04:38:53 +0000

    Librerias USART (USART.c, USART.h) con funcionalidad basica (Inicializacion completa, enviar y recibir datos). Falta funcionalidades de control de errores (Bit de paridad, envio de Strings, etc)

--- AVR/ControlParqueadero644/includes/USART.c
+++ AVR/ControlParqueadero644/includes/USART.c
@@ -47,22 +47,131 @@
  * parametro y en el modo selecccionado (Sincrono o asincrono), configurando la velocidad
  * y demás parámetros con la definicion de las constantes en el archivo de definiciones
  *
- * @PARAMS: uint8_t puerto -> Numero del modulo Usart (Generalmente 0 o 1)
- *          uint8_t as     -> Establece el modulo Sincrono o Asincrono
+ * @PARAMS: Ninguno
  * @PRE:    Los parametros se encuentran bien definidos
  * @POST:   Se inicializa el puerto USART seleccionado en el modo seleccionado, quedando
  *          listo para enviar y recibir datos.
  * @RETURN: Void
  */
-void USART_init(uint8_t puerto, uint8_t as){
+void USART0_init(){
 
-	// Nos aseguramos que la USART se encuentre inactiva
+	// Nos aseguramos que la USART se encuentre inactiva y configuracion default
+	UCSR0A = 0x20;
 	UCSR0B = 0x00;
-	UCSR0A = 0x00;
+	UCSR0C = 0x06;
+	
 
 	// Asignamos el valor del valor de la tasa de transmision en baudios
 	UBRR0L=UBRRVAL;			//Parte baja del byte
-	UBRR0H=(UBRRVAL>>8);	//PArte alta del byte
+	UBRR0H=(UBRRVAL>>8);	//Parte alta del byte
+
+	if(ASY_SYN){
+		//Configurado en modo Asincrono
+		if(SPEED_2X){UCSR0A |= _BV(U2X0);}
+		
+	}else{
+		// Configurado en modo Sincrono
+		UCSR0C |= (1<<6);		
+	}
+
+	switch(CHAR_SIZE){
+		case 5: UCSR0C &= ~(_BV(UCSZ00)|_BV(UCSZ01));break;
+		case 6: UCSR0C &= ~(_BV(UCSZ01));break;
+		case 7: UCSR0C &= ~(_BV(UCSZ00));break;
+		case 9: UCSR0B|= _BV(UCSZ02);break;
+		default:break;
+	}
+
+	UCSR0B |= (INT_TX_COMPLETA)?_BV(TXCIE0):0;
+	UCSR0B |= (INT_RX_COMPLETA)?_BV(RXCIE0):0;
+
+	switch(CONTROL_PARIEDAD){
+		case 1: UCSR0C |= _BV(UPM01); break;
+		case 2: UCSR0C |= (_BV(UPM01)|_BV(UPM00)); break;
+		default: break;
+	}
+
+	if(NUM_STOP==2) UCSR0C |= _BV(USBS0);
+	if(CLK_POL) UCSR0C |= _BV(UCPOL0);
+}
 
 
+
+/**
+ * USART0_EnableTx
+ * @PARAMS: Ninguno
+ * @PRE:    El puerto USART0 se encuentra inicializado
+ * @POST:   El puerto USART0 se habilita para transmision 
+ * @RETURN: Void
+ */
+void USART0_EnableTx(){
+	UCSR0B |= _BV(TXEN0);
+}
+
+/**
+ * USART0_DisableTx
+ * @PARAMS: Ninguno
+ * @PRE:    El puerto USART0 se encuentra inicializado
+ * @POST:   El puerto USART0 se deshabilita para transmision 
+ * @RETURN: Void
+ */
+void USART0_DisableTx(){
+	UCSR0B &= ~_BV(TXEN0);
+}
+
+/**
+ * USART0_EnableRx
+ * @PARAMS: Ninguno
+ * @PRE:    El puerto USART0 se encuentra inicializado
+ * @POST:   El puerto USART0 se habilita para Recepsion 
+ * @RETURN: Void
+ */
+void USART0_EnableRx(){
+	UCSR0B |= _BV(RXEN0);
+}
+
+
+/**
+ * USART0_DisableRx
+ * @PARAMS: Ninguno
+ * @PRE:    El puerto USART0 se encuentra inicializado
+ * @POST:   El puerto USART0 se deshabilita para Recepcion 
+ * @RETURN: Void
+ */
+ void USART0_DisableRx(){
+ 	UCSR0B &= ~_BV(RXEN0);
+ }
+
+
+ /**
+  * USART0_Enviar
+  * @PARAMS uint8_t dato -> Dato que se desea enviar
+  * @PRE:   El puerto USART0 se encuentra configurado y habilitado para el envio de mensajes+
+  * @POST:  Se coloca el mensaje en el buffer de salida de la USART0
+  * @RETURN: Void
+  */
+void USART0_Enviar(uint8_t dato){
+
+	// Espera a que se se termina la transmision anterior si esta en curso
+	while ( !( UCSR0A & (1<<UDRE0)) );
+
+	// Coloca el dato a enviar en el buffer de salida
+	UDR0 = dato;
+
+}
+
+
+/**
+ * USART0_Recibir
+ * @PARAMS: Ninguno
+ * @PRE:    El puerto USART0 se encuentra configurado y habilitado para la recepcion de mensajes
+ * @POST:   Se retorno el mensaje que se almaceno en el buffer de entrada de la USART0
+ * @RETURN: uint8_t dato -> El dato recibido en la transmision
+ */
+uint8_t USART0_Recibir(){
+
+	// Se espera a que se reciba un mensaje si esta no ha llegado todavia
+	//while ( !(UCSR0A & (1<<RXC0)) );
+
+	return UDR0;
 }
