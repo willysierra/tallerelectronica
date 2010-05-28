e0e105f06d1bcd9d0d6bbd0291e2477566219ed6
me.vieda372 <me.vieda372@3c8b30a8-bd7b-6e8a-6419-ac650d15a375>
2010-05-28 12:02:38 +0000

    Libreria para modulo SPI  (SPI.c, SPI.h) completos pero o probaDOS. Inicio de las librerias para protocolo SPI y SDCard.

---
+++ AVR/ControlParqueadero644/includes/I2C.c
@@ -0,0 +1,41 @@
+/**
+ * -------------------------------------------------------------------
+ * Copyright (c) 2010 - Todos los derechos reservados
+ * Manuel Eduardo Vieda Salomon <me.vieda372@gmail.com
+ * Ingeniero Electronico, Ingeniero de Sistemas y Computacion
+ * Universidad de los Andes. Bogota, Colombia.
+ * -------------------------------------------------------------------
+ *
+ * Archivo:				I2C.c
+ * Fecha Creacion:		Mayo  28 de 2010
+ * Fecha Modificacion:	Mayo  28 de 2010
+ * Version (Revision):	0.1 (1)
+ *
+ * Descripcion:	Libreria que facilita el uso del modulo de comunicación
+ *              bajo el protocolo I2C/TWI de los microcontroladores ATmega
+ *
+ *
+ * --------------------------------------------------------------------
+ * LICENCIA:
+ * 
+ * Se encuentra totalmente permitido el uso y distribucion en forma de codigo y
+ * binario bajo la condicion de cumplir con las siguientes condiciones de uso:
+ *
+ *   - Toda redistribucion del codigo fuente debe mantener sin ningun cambio la
+ *     presente notificacion de copyright, la lista de condiciones y la descripcion
+ *     del archivo.
+ *
+ *   - Ni el nombre del poseedor del presente copyright o el nombre
+ *     de cualquier colaborador puede ser usado para la promocion de productos
+ *     derivados de este programa/codigo sin una autorizacion escrita y especifica
+ *     por parte del autor.
+ *
+ *   - Si considera que este contenido le ha sido bastante util, puedes invitarme
+ *     a una ronda de cervezas. (The Beer-Ware License, Revision 42)
+ * ---------------------------------------------------------------------
+ */
+
+ #include "defines.h"
+ #include <stdint.h>
+ #include <avr/io.h>
+ #include "I2C.h"
