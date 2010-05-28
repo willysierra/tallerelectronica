23fc04180a294e3c00e653b912da1cfc1d60bc73
me.vieda372 <me.vieda372@3c8b30a8-bd7b-6e8a-6419-ac650d15a375>
2010-05-28 23:22:42 +0000

    Inicializacion del proyecto del segundo microcontrolador. librerias SPI y SD continuan en desarrollo.

---
+++ AVR/ControlParqueadero168/ControlParqueadero168.c
@@ -0,0 +1,85 @@
+/**
+ * -------------------------------------------------------------------
+ * Universidad de los Andes - Facultad de Ingenieria
+ * Departamento de Ingenieria Electrica y Electronica
+ * Proyecto Taller de Electrónica
+ * -------------------------------------------------------------------
+ *
+ * Archivo:				ControlParqueadero168.c
+ * Fecha Creacion:		Mayo 28 de 2010
+ * Fecha Modificacion:	Mayo 28 de 2010
+ * Version (Revision):	0.1 (1)
+ *
+ * Descripcion:	Control principal proyecto Parqueadero Edificio SantoDomingo
+ *               Microcontrolador Esclavo
+ *
+ */
+
+/* Exportación de Librerias */
+
+#include "includes/defines.h"
+
+#include <stdio.h>			// Standard IO facilities	
+#include <stdbool.h>		// Standar Boolean facilities
+#include <avr/io.h>			// AVR device-specific IO definitions
+#include <util/delay.h>		// Convenience functions for busy-wait delay loops
+#include <avr/interrupt.h>	// Interrupts handling
+#include <avr/pgmspace.h>	// Program Space Utilities
+#include <avr/eeprom.h>		// EEPROM Handling
+
+#include "includes/SPI.h"
+#include "includes/I2C.h"
+#include "includes/SDCard.h"
+
+//  ------------------------------------------------------------------------
+//	VARIABLES GLOBALES DEL SISTEMA / BUFFERS
+//  ------------------------------------------------------------------------
+
+
+//  ------------------------------------------------------------------------
+//	DECLARACION DE METODOS / PROCEDIMIENTOS INTERNOS
+//  ------------------------------------------------------------------------
+void initHardware();
+
+
+//  ------------------------------------------------------------------------
+//	PROGRAMA PRINCIPAL DEL SISITEMA
+//  ------------------------------------------------------------------------
+
+int main(void) {
+
+
+	_delay_ms(40);
+	initHardware();
+
+
+	while(1){
+
+	}
+
+}
+
+
+/**
+ * Metodo que se encarga de la inicializacion del hardware del proyecto. Dentro del 
+ * hardware se encuentran tanto los periféricos externos como los internos del micro.
+ * @PARAM:  Ninguno
+ * @PRE:    Ninguno
+ * @POST:   Se inicializa el hardware. Listo para uso rutinario
+ * @RETURN: Void
+ */
+void initHardware(){
+
+DDRD = 0xFF;
+PORTD = 0xFF;
+PORTD  = 0xAA;
+
+	
+	SPI_init();
+	
+PORTD = 0x55;
+SD_init();
+
+
+}
+
